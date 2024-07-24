f7342c799a7e5acf70d01f631d515cd8f1b8f11a
Shubham Singh <singhshubham982047@gmail.com>
2024-07-25 00:01:48 +0530

    add main component

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className='main '>
+      <div className="nav ">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="user" className='w-[40px] rounded-full' />
+      </div>
+      <div className="main-container ">
+        <div className="greet ">
+          <p>
+            <span >Hello, Shubham</span>
+          </p>
+          <p>How can I help you today ?</p>
+        </div>
+        <div className="cards ">
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="compass" />
+          </div>
+          <div className="card">
+            <p>Breifly summarize this concept: urban planning</p>
+            <img src={assets.bulb_icon} alt="compass" />
+          </div>
+          <div className="card">
+            <p>Brainstrom team bonding activities for our work retreat</p>
+            <img src={assets.message_icon} alt="compass" />
+          </div>
+          <div className="card">
+            <p>Improve the readability of the following code</p>
+            <img src={assets.code_icon} alt="compass" />
+          </div>
+        </div>
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder='Enter a prompt here' />
+            <div >
+              <img src={assets.gallery_icon} alt="gallery" />
+              <img src={assets.mic_icon} alt="mic" />
+              <img src={assets.send_icon} alt="send" />
+            </div>
+          </div>
+          <p className='bottom_info'>Gemini may display inaccurate info, including about people, so double-check its responses</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -6,32 +6,32 @@ const Sidebar = () => {
   const [extended, setExtended] = useState(false)
 
   return (
-    <div className='sidebar min-h-[100vh] inline-flex flex-col justify-between bg-[#f0f4f9] py-[25px] px-[15px]'>
+    <div className='sidebar min-h-[100vh] inline-flex flex-col justify-between bg-[#f0f4f9] py-[25px] px-[15px] text-sm'>
       <div className="top">
         <img className='menu block ml-[10px] cursor-pointer ' src={assets.menu_icon} alt="menu" onClick={() => setExtended(prev => !prev)} />
-        <div className='new-chat mt-5 inline-flex items-center gap-[10px] py-3 px-4 bg-[#e6eaf1] rounded-full text-gray-500 cursor-pointer'>
-          <img src={assets.plus_icon} alt="new-chat" className='' />
+        <div className='new-chat mt-5 inline-flex items-center gap-[10px] py-[8px] px-4 bg-[#e6eaf1] rounded-full text-gray-500 cursor-pointer'>
+          <img src={assets.plus_icon} alt="new-chat" />
           {extended && <p>New Chat</p>}
         </div>
         {extended && <div className='recent flex flex-col '>
           <p className='recent-title mt-[30px] mb-[20px]'>Recent</p>
-          <div className="recent-entry flex  items-center gap-[10px] p-[10px] pr-10 rounded-full text-[#282828] cursor-pointer hover:bg-[#e2e6eb]">
+          <div className="recent-entry flex  items-start gap-[10px] p-[10px] pr-10 rounded-full text-[#282828] cursor-pointer hover:bg-[#e2e6eb]">
             <img src={assets.message_icon} alt="message" />
             <p>What is react...</p>
           </div>
         </div>}
       </div>
       <div className="bottom flex flex-col">
-        <div className="bottom-item recent-entry flex items-center gap-[10px] p-[10px] pr-10 rounded-full text-[#282828] cursor-pointer  hover:bg-[#e2e6eb]  ">
+        <div className="bottom-item recent-entry flex items-start gap-[10px] p-[10px] pr-10 rounded-full text-[#282828] cursor-pointer  hover:bg-[#e2e6eb]  ">
           <img src={assets.question_icon} alt="question" />
           {extended && <p>Help</p>}
         </div>
-        <div className="bottom-item recent-entry flex items-center gap-[10px] p-[10px] pr-10 rounded-full text-[#282828] cursor-pointer  hover:bg-[#e2e6eb] ">
-          <img src={assets.history_icon} alt="question" />
+        <div className="bottom-item recent-entry flex items-start gap-[10px] p-[10px] pr-10 rounded-full text-[#282828] cursor-pointer  hover:bg-[#e2e6eb] ">
+          <img src={assets.history_icon} alt="history" />
           {extended && <p>Activity</p>}
         </div>
-        <div className="bottom-item recent-entry flex items-center gap-[10px] p-[10px] pr-10 rounded-full text-[#282828] cursor-pointer  hover:bg-[#e2e6eb] ">
-          <img src={assets.setting_icon} alt="question" />
+        <div className="bottom-item recent-entry flex items-start gap-[10px] p-[10px] pr-10 rounded-full text-[#282828] cursor-pointer  hover:bg-[#e2e6eb] ">
+          <img src={assets.setting_icon} alt="settings" />
           {extended && <p>Settings</p>}
         </div>
       </div>
