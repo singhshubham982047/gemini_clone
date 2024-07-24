cfc6618a879f7744e6e6e2a91c15e636e04320b7
Shubham Singh <singhshubham982047@gmail.com>
2024-07-24 15:53:49 +0530

    add sidebar component

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from "../../assets/assets"
 
 const Sidebar = () => {
+
+  const [extended, setExtended] = useState(false)
+
   return (
-    <div className='sidebar'>
-      <div className="top"><img src={assets.menu_icon} alt="menu" /></div>
-      <div className="bottom"></div>
+    <div className='sidebar min-h-[100vh] inline-flex flex-col justify-between bg-[#f0f4f9] py-[25px] px-[15px]'>
+      <div className="top">
+        <img className='menu block ml-[10px] cursor-pointer ' src={assets.menu_icon} alt="menu" onClick={() => setExtended(prev => !prev)} />
+        <div className='new-chat mt-5 inline-flex items-center gap-[10px] py-3 px-4 bg-[#e6eaf1] rounded-full text-gray-500 cursor-pointer'>
+          <img src={assets.plus_icon} alt="new-chat" className='' />
+          {extended && <p>New Chat</p>}
+        </div>
+        {extended && <div className='recent flex flex-col '>
+          <p className='recent-title mt-[30px] mb-[20px]'>Recent</p>
+          <div className="recent-entry flex  items-center gap-[10px] p-[10px] pr-10 rounded-full text-[#282828] cursor-pointer hover:bg-[#e2e6eb]">
+            <img src={assets.message_icon} alt="message" />
+            <p>What is react...</p>
+          </div>
+        </div>}
+      </div>
+      <div className="bottom flex flex-col">
+        <div className="bottom-item recent-entry flex items-center gap-[10px] p-[10px] pr-10 rounded-full text-[#282828] cursor-pointer  hover:bg-[#e2e6eb]  ">
+          <img src={assets.question_icon} alt="question" />
+          {extended && <p>Help</p>}
+        </div>
+        <div className="bottom-item recent-entry flex items-center gap-[10px] p-[10px] pr-10 rounded-full text-[#282828] cursor-pointer  hover:bg-[#e2e6eb] ">
+          <img src={assets.history_icon} alt="question" />
+          {extended && <p>Activity</p>}
+        </div>
+        <div className="bottom-item recent-entry flex items-center gap-[10px] p-[10px] pr-10 rounded-full text-[#282828] cursor-pointer  hover:bg-[#e2e6eb] ">
+          <img src={assets.setting_icon} alt="question" />
+          {extended && <p>Settings</p>}
+        </div>
+      </div>
     </div>
   )
 }
