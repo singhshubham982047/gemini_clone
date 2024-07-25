1bb606b8351ff279a1a36d4f596ccf8cc20537ce
Shubham Singh <155379514+singhshubham982047@users.noreply.github.com>
2024-07-25 18:51:57 +0530

    fix typo

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -24,7 +24,7 @@ const Main = () => {
         {!showResult ? <>
           <div className="greet ">
             <p>
-              <span >Hello, Shubham</span>
+              <span >Hello, User</span>
             </p>
             <p>How can I help you today ?</p>
           </div>
